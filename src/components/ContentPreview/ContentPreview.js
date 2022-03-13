1f6363635c252d8987b08d1699cc73aea664d2fe
ted-millie <ted@millie.town>
2022-03-14 00:54:46 +0900

    FOUC현상 해결을 위해 커스텀 Document 생성

--- src/components/ContentPreview/ContentPreview.js
+++ src/components/ContentPreview/ContentPreview.js
@@ -9,7 +9,7 @@ const ContentPreview = () => {
           <img src="https://apex-software.imgix.net/logs/site/light-with-logs.png?auto=format&w=1000&dpr=2" alt="" />
         </Thumbnail> */}
         <Metadata>
-          <Title>이미지 최적화를 위한 WebP 포맷 적용 이야기</Title>
+          <Title>이미지 최적화를 위한 WebP 포맷 적용 이야기#</Title>
           <Excerpt>레진코믹스는 다양한 웹툰을 글로벌 서비스하고 있는 회사입니다. 저희 개발구성원들은 항상 더 빠르고 안정적인 서비스를 하기 위해 각자 맡은 영역에서 최선을 다하고 있습니다. 그 노력의 일환으로 백엔드 관점에서 차세대 포맷인 WebP를 적용하면서 겪었던 경험담을 공유하려 합니다. WebP 란 무엇인가? 2010년 구글이 만든 이미지 포맷으로 홈페이지에서 JPEG(손실)나 PNG(비손실) 포맷대비 30% 정도 더 사이즈가 작다고 설명하고 있습니다.</Excerpt>
           <ReadMore>자세히 보기</ReadMore>
         </Metadata>
