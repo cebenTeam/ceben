3c37409a09e65391106d38667dffaf07b6d5e47e
ted-millie <ted@millie.town>
2022-03-21 19:06:14 +0900

    Dock 디자인 변경

--- src/components/Dock/Dock.js
+++ src/components/Dock/Dock.js
@@ -1,11 +1,12 @@
 import Link from 'next/link'
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
 
 const Dock = () => {
   return (
     <Wrapper>
       <Link href="/" passHref>
-        <Launch>
+        <Launch whileHover={{ width: 80, height: 80 }}>
           <img src="/assets/icons/home.svg" alt="" />
           <Tooltip>홈</Tooltip>
           <Dot></Dot>
@@ -13,31 +14,26 @@ const Dock = () => {
       </Link>
       <HR />
       <Link href="/" passHref>
-        <Launch>
+        <Launch whileHover={{ width: 80, height: 80 }}>
           <img src="/assets/icons/docs.svg" alt="" />
           <Tooltip>포스트</Tooltip>
         </Launch>
       </Link>
       <Link href="/" passHref>
-        <Launch>
+        <Launch whileHover={{ width: 80, height: 80 }}>
           <img src="/assets/icons/project.svg" alt="" />
           <Tooltip>프로젝트</Tooltip>
         </Launch>
       </Link>
-      {/* <Link href="/" passHref>
-        <Launch>
-          <img src="/assets/icons/tutorial.svg" alt="" />
-        </Launch>
-      </Link> */}
       <Link href="/" passHref>
-        <Launch>
+        <Launch whileHover={{ width: 80, height: 80 }}>
           <img src="/assets/icons/github.svg" alt="" />
           <Tooltip>깃허브</Tooltip>          
         </Launch>
       </Link>
       <HR />
       <Link href="/" passHref>
-        <Launch type="button" as="button">
+        <Launch whileHover={{ width: 80, height: 80 }}>
           <img src="/assets/icons/sun.svg" alt="" />
           <Tooltip>다크모드</Tooltip>
         </Launch>
@@ -51,8 +47,9 @@ const Wrapper = styled.nav`
   left: 50%;
   bottom: 0;
   display: flex;
-  align-items: center;
-  gap: 4px;
+  align-items: flex-end;
+  height: 68px;
+  gap: 8px;
   padding: 10px;
   border-radius: 20px;
   transform: translate(-50%, -24px);
@@ -63,20 +60,20 @@ const Wrapper = styled.nav`
 const Tooltip = styled.span`
   position: absolute;
   left: 50%;
-  top: -100%;
+  top: 0;
   padding: 4px 8px;
   font-size: 12px;
   white-space: nowrap;
   opacity: 0;
-  transform: translate(-50%, 10px);
-  transition: all .24s cubic-bezier(0,0,.2,1);  
+  transform: translate(-50%, calc(-100%));
+  transition: all .24s .4s cubic-bezier(0,0,.2,1);  
   border: 1px soild #dedede;
   border-radius: 4px;
   background-color: rgba(176, 179, 188, 0.1);
   backdrop-filter: blur(40px);
 `
 
-const Launch = styled.a`
+const Launch = styled(motion.a)`
   position: relative;
   display: flex;
   align-items: center;
@@ -84,9 +81,16 @@ const Launch = styled.a`
   width: 48px;
   height: 48px;
   color: var(--text-color);
+  background-color: #f0f0f2;
+  border-radius: 12px;
   &:hover ${Tooltip} {
     opacity: 1;
-    transform: translate(-50%, 0);
+    transition: all .24s .6s cubic-bezier(0,0,.2,1);  
+    transform: translate(-50%, calc(-120%));
+  }
+  img {
+    width: 50%;
+    height: 50%;
   }
 `
 
@@ -104,7 +108,7 @@ const Dot = styled.span`
 const HR = styled.hr`
   width: 1px;
   height: 30px;
-  margin: 0px 4px;
+  margin: 9px 2px;
   border: 0px;
   background-color: #dedede;
 `
